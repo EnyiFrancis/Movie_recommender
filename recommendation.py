1c45054f3a397f0a8c99025c47277ad1705f6bb5
Francis Owuna Enyi <97532613+EnyiFrancis@users.noreply.github.com>
2022-09-18 08:19:45 +0200

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -7,8 +7,8 @@ contains various implementations for recommending movies
 
 import pandas as pd
 import numpy as np
-from utils import nmf_model, neighbor_model, movies, ratings, movie_to_id, sklearn
-
+from utils import nmf_model, neighbor_model, movies, ratings, movie_to_id
+import sklearn
 
 
 
